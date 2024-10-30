438f5768375804ad31e6b1a141584cde0536e324
Zivanovic8 <zivanova@dickinson.edu>
2024-10-30 14:44:03 -0400

    Added API categories but it is not finished

--- src/app/starting-page/starting-page.component.ts
+++ src/app/starting-page/starting-page.component.ts
@@ -11,22 +11,24 @@ import { FormsModule } from '@angular/forms';
   styles: ["node_modules/bootstrap/dist/css/bootstrap.min.css"]
 })
 export class StartingPageComponent {
-  @Input() categoryList!: String[];
+  @Input() categoryList!: number[];
   @Input() difficulties!: String[];
 // this used to emit a value when an some event happens in out case a button clicked 
+  @Output() startQuiz = new EventEmitter<{ category: number; difficulty: string }>(); // Emit selected values
   @Output() switchPage = new EventEmitter<string>();
-  selectedCategory: string = "";
+  selectedCategory: number = 0;
   selectedDifficulty: string = "";
   isVisible: boolean = false;
 
   switchToQuestionPage() {
-    if((this.selectedCategory != "") && (this.selectedDifficulty != "")) {
-      console.log(this.selectedCategory)
-      console.log(this.selectedDifficulty)
-      console.log('pressed');
-      this.switchPage.emit('questionPage');
+    if (this.selectedCategory && this.selectedDifficulty) {
+      console.log(this.selectedCategory, this.selectedDifficulty);
+      this.startQuiz.emit({
+        category: this.selectedCategory,
+        difficulty: this.selectedDifficulty,
+      });
     } else {
-      this.isVisible = true
+      this.isVisible = true;
     }
   }
 
