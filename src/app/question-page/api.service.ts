cf880c5e9780007728346b1cfaf3003d6b596ee5
EliasBerhe <berhee@dickinson.edu>
2024-10-31 17:47:58 -0400

    fixed api catered calls

--- src/app/question-page/api.service.ts
+++ src/app/question-page/api.service.ts
@@ -7,13 +7,22 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'https://opentdb.com/api.php?amount=5&category=11&difficulty=easy&type=multiple&encode=url3986'; // API URL
-
-  constructor(private http: HttpClient) {}
+  //private apiUrl = 'https://opentdb.com/api.php?amount=5&category=11&difficulty=easy&type=multiple&encode=url3986'; // API URL
+  map: Map<string, number> = new Map();
+  
+  constructor(private http: HttpClient) {
+    this.map.set("General Knowledge",9);
+    this.map.set("Sports",21);
+    this.map.set("Film", 11);
+    this.map.set("Science", 17);
+  }
+  
 
   // Fetch questions from the API
-  getQuestions(): Observable<any[]> {
-    return this.http.get<any>(this.apiUrl).pipe(
+  getQuestions(category: string, difficulty:string): Observable<any[]> {
+    const apiUrl = `https://opentdb.com/api.php?amount=5&category=${this.map.get(category)}&difficulty=${difficulty}&type=multiple&encode=url3986`;
+    console.log(apiUrl);
+    return this.http.get<any>(apiUrl).pipe(
       map(response => response.results) // Extract the 'results' array from the response
     );
   }
